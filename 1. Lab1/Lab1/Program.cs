4d0f96cdb20bb789a1da14927e6b3472d5b92824
hardikeprajapati <ehardik92@gmail.com>
2018-08-07 10:33:46 +0530

    ConsoleApplication
    Simple Console Based Application in C# for Beginner.

---
+++ 1. Lab1/Lab1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string name = string.Empty;
+                Console.WriteLine("Enter name :");
+                name = Console.ReadLine();
+
+                if (name.Length == 0)
+                {
+                    Console.WriteLine("Name is required.");
+                    Console.Read();
+                    return;
+                }
+
+                int age = 0;
+                Console.WriteLine("Enter age :");
+                age = Convert.ToInt16(Console.ReadLine());
+                if (age <18)
+                {
+                    Console.WriteLine("Not eligible,only >18 can access.");
+                    Console.Read();
+                    return;
+                }
+                Console.WriteLine("Hello," + "Your name is: " + "\n" + name + "\n" + "Your age is : " + "\n" + age);
+                Console.Read();
+            }
+            catch
+            {
+                Console.WriteLine("Please try..invalid data");
+                Console.Read();
+            }
+        }
+    }
+}

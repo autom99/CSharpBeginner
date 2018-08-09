2422ad2bb90d1157c1a1ad94a8887db0fe972700
hardikeprajapati <ehardik92@gmail.com>
2018-08-09 15:46:56 +0530

    Add files via upload

---
+++ 10. ConsoleShadowing/ConsoleShadowing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleShadowing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class1 obj1 = new class1();
+            class2 obj2 = new class2();
+            //Datatype int
+            obj1.InvoiceNumber = 123;
+            //Datatype object.
+            obj2.InvoiceNumber = "123456"; 
+        }
+
+        class class1
+        {
+            public int InvoiceNumber { set; get; }
+        }
+
+        class class2 : class1
+        {
+            public new object InvoiceNumber { set; get; }
+        }
+    }
+
+
+}

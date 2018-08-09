8a3166c0cdd2607b2e8493ab343c75037ddffae7
hardikeprajapati <ehardik92@gmail.com>
2018-08-09 15:45:16 +0530

    Add files via upload

---
+++ 8. Out_and_Ref/src/Out_and_Ref/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Out_and_Ref
+{
+    public class Program
+    {
+        #region ref keyword: two way func calling as reference.
+        ////Caller Func
+        //public static void Main(string[] args)
+        //{
+        //    int OutSideVar = 20;
+        //    SomeFunction(ref OutSideVar);
+
+        //    Console.WriteLine(OutSideVar);
+        //}
+        ////Callee Func
+        //static void SomeFunction(ref int InsideVar)
+        //{
+        //    InsideVar = InsideVar + 10;
+        //}
+        #endregion
+
+        #region out keyword: only one way Callee to caller func calling as reference.
+        //Caller Func
+        public static void Main(string[] args)
+        {
+            int OutSideVar = 20;
+            SomeFunction(out OutSideVar);
+
+            Console.WriteLine(OutSideVar);
+        }
+        //Callee Func
+        static void SomeFunction(out int InsideVar)
+        {
+            InsideVar = 0;
+            InsideVar = InsideVar + 10;
+        }
+        #endregion
+    }
+}
